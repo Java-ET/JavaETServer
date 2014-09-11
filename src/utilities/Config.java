7aaa146d9ec4bf0e16b374c22cfb0839f70c5e08
Ziggeh <ziggy@projectswg.com>
2014-09-11 22:11:17 +0200

    Fixed a resource leak with config files

--- src/utilities/Config.java
+++ src/utilities/Config.java
@@ -8,20 +8,15 @@ import java.util.Scanner;
 public class Config {
 	private Map<String, Object> entries;
 	
-	public Config(String configPath) {
-		File file = new File(configPath);
-		if (!file.exists()) // We shouldn't continue loading if the .cfg for the server's missing.
-			return;
-		
+	public Config(String configPath) {	
 		entries = new HashMap<String, Object>();
-		Scanner fileReader = new Scanner(file.getPath());	// Will getPath() return the correct path?
+		Scanner fileReader = new Scanner(new File(configPath).getPath());	// Will getPath() return the correct path?
 		
 		while(fileReader.hasNextLine()) {
 			String entry  = fileReader.nextLine();
-			
-			entries.put(entry.split("=")[0], entry.split("=")[1]);	// Cache the config. More efficient than reading it over and over.
+			entries.put(entry.split("=")[0], entry.split("=")[1]);	// Cache the config. More efficient than reading it over and over.			
 		}
-		
+		fileReader.close();
 	}
 	
 	public int getInt(String key) {
