b7f2c70910033c9325884cfc9ef8a2aaf9203fab
Ziggeh <ziggy@projectswg.com>
2014-09-20 06:33:51 +0200

    Removed duplicate Config class, added J-ETConfig lib

--- src/main/ETCore.java
+++ src/main/ETCore.java
@@ -1,20 +1,34 @@
 package main;
 
-import utilities.Config;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Vector;
+
+import player.Player;
+import items.weapons.Weapon;
+import utils.Config;
+import utilities.Jython;
 
 public class ETCore {
 	private static int port;
 	private static String serverName;
 	private static byte maxPlayers;
+	private Vector<Player> playerList;
 	
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		Config server = new Config("server.cfg");
-		
+		Map<String, Long> serverPk3hashes = new HashMap<String, Long>();
 		serverName = server.getString("serverName");
 		port = server.getInt("port");
 		maxPlayers = server.getByte("maxPlayers");
 		
-		System.out.println("Launched gameserver: " + serverName + " with port " + port + " and a max of " + maxPlayers + " playes.");
+		Weapon weap = new Weapon();
+		Jython.callScript("scripts/weapons/guns/", "mp40", "setup", weap);
+		
+		ETConsole serverConsole = new ETConsole();
+		serverConsole.println("Launched gameserver: " + serverName + " with port " + port + " and a max of " + maxPlayers + " players.");
+		
 	}
 
 }
